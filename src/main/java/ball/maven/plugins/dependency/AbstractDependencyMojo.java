43dda4fa04920fe94aee2b2d5ef4d7585183cddc
Allen D. Ball <ball@hcf.dev>
2022-07-10 19:28:03 -0700

    Scaffolded analysis logic.

--- src/main/java/ball/maven/plugins/dependency/AbstractDependencyMojo.java
+++ src/main/java/ball/maven/plugins/dependency/AbstractDependencyMojo.java
@@ -18,13 +18,21 @@ package ball.maven.plugins.dependency;
  * limitations under the License.
  * ##########################################################################
  */
+import ball.annotation.CompileTimeCheck;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.regex.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
+import static java.util.stream.Collectors.toCollection;
 import static lombok.AccessLevel.PROTECTED;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
 
 /**
  * Abstract base class for dependency {@link org.apache.maven.plugin.Mojo}s.
@@ -39,4 +47,44 @@ import static lombok.AccessLevel.PROTECTED;
  */
 @NoArgsConstructor(access = PROTECTED) @Getter @ToString @Slf4j
 public abstract class AbstractDependencyMojo extends AbstractMojo {
+    @CompileTimeCheck
+    private static final Pattern PATTERN = Pattern.compile("[\\p{Punct}\\p{Space}]+");
+
+    private static final String COMPILE = "compile";
+    private static final String PROVIDED = "provided";
+    private static final String RUNTIME = "runtime";
+    private static final String SYSTEM = "system";
+    private static final String TEST = "test";
+    private static final String INCLUDE_SCOPE = COMPILE + "," + RUNTIME + "," + SYSTEM + "," + PROVIDED;;
+    private static final String EXCLUDE_SCOPE = EMPTY;
+
+    @Parameter(defaultValue = INCLUDE_SCOPE, property = "dependency.includeScope")
+    private String includeScope = INCLUDE_SCOPE;
+
+    @Parameter(defaultValue = EXCLUDE_SCOPE, property = "dependency.excludeScope")
+    private String excludeScope = EXCLUDE_SCOPE;
+
+    /**
+     * Method to get the specified scope.
+     *
+     * @return  The scopes specified by the Mogo parameters.
+     */
+    protected Set<String> getScope() {
+        TreeSet<String> scope =
+            PATTERN.splitAsStream(includeScope)
+            .filter(StringUtils::isNotBlank)
+            .collect(toCollection(() -> new TreeSet<>(String.CASE_INSENSITIVE_ORDER)));
+
+        PATTERN.splitAsStream(excludeScope)
+            .filter(StringUtils::isNotBlank)
+            .forEach(t -> scope.remove(t));
+
+        if (scope.isEmpty()) {
+            log.warn("Specified scope is empty");
+            log.debug("    includeScope = {}", includeScope);
+            log.debug("    excludeScope = {}", excludeScope);
+        }
+
+        return scope;
+    }
 }
--- src/main/java/ball/maven/plugins/dependency/AnalysisMojo.java
+++ src/main/java/ball/maven/plugins/dependency/AnalysisMojo.java
@@ -18,23 +18,39 @@ package ball.maven.plugins.dependency;
  * limitations under the License.
  * ##########################################################################
  */
+import ball.annotation.CompileTimeCheck;
+import java.io.IOException;
+import java.net.JarURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 import java.util.regex.Pattern;
+import java.util.zip.ZipException;
 import javax.inject.Inject;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.ArtifactUtils;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
-import static java.util.stream.Collectors.toCollection;
-import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.toSet;
 import static org.apache.maven.plugins.annotations.LifecyclePhase.GENERATE_RESOURCES;
 import static org.apache.maven.plugins.annotations.ResolutionScope.TEST;
 
@@ -53,44 +69,89 @@ import static org.apache.maven.plugins.annotations.ResolutionScope.TEST;
       defaultPhase = GENERATE_RESOURCES, requiresProject = true)
 @NoArgsConstructor @ToString @Slf4j
 public class AnalysisMojo extends AbstractDependencyMojo {
-    private static final String COMPILE = "compile";
-    private static final String PROVIDED = "provided";
-    private static final String RUNTIME = "runtime";
-    private static final String SYSTEM = "system";
-    private static final String TEST = "test";
-    private static final String INCLUDE_SCOPE = COMPILE + "," + RUNTIME;
-    private static final String EXCLUDE_SCOPE = EMPTY;
+    private static Comparator<? super Artifact> COMPARATOR =
+        Comparator.comparing(ArtifactUtils::key, String.CASE_INSENSITIVE_ORDER);
 
-    @Parameter(defaultValue = INCLUDE_SCOPE, property = "dependency.includeScope")
-    private String includeScope = INCLUDE_SCOPE;
-
-    @Parameter(defaultValue = EXCLUDE_SCOPE, property = "dependency.excludeScope")
-    private String excludeScope = EXCLUDE_SCOPE;
+    @CompileTimeCheck
+    private static final Pattern PATTERN =
+        Pattern.compile("(?i)^(META-INF/versions/[\\p{Digit}]+/)?(?<path>.*)[.]class$");
 
     @Inject private MavenProject project = null;
 
-
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
+        Set<String> scope = getScope();
+        Map<Artifact,Set<String>> map =
+            project.getArtifacts().stream()
+            .filter(t -> scope.contains(t.getScope()))
+            .collect(toMap(k -> k, v -> classesIn(v), (t, u) -> u, () -> new TreeMap<>(COMPARATOR)));
+        List<Artifact> list = new LinkedList<>(map.keySet());
+
+        while (! list.isEmpty()) {
+            Artifact artifact0 = list.remove(0);
+            Set<String> set0 = map.get(artifact0);
+
+            for (Artifact artifactN : list) {
+                Set<String> setN = map.get(artifactN);
+
+                if (! Collections.disjoint(set0, setN)) {
+                    log.info("");
+
+                    if (set0.equals(setN)) {
+                        log.info("{} and {} contain the same class entries", artifact0, artifactN);
+                    } else if (set0.contains(setN)) {
+                        log.info("{} fully contains {} class entries", artifact0, artifactN);
+                    } else if (setN.contains(set0)) {
+                        log.info("{} fully contains {} class entries", artifactN, artifact0);
+                    } else {
+                        Set<String> common = new TreeSet<>();
+
+                        common.addAll(set0);
+                        common.retainAll(setN);
+
+                        log.info("{} and {} contain {} common class entries", artifact0, artifactN, common.size());
+                    }
+                } else {
+                    log.debug("");
+                    log.debug("{} and {} contain disjoint class entries", artifact0, artifactN);
+                }
+            }
+        }
     }
 
-    private Set<String> getScope() {
-        Pattern pattern = Pattern.compile("[\\p{Punct}\\p{Space}]+");
-        TreeSet<String> scope =
-            pattern.splitAsStream(includeScope)
-            .filter(StringUtils::isNotBlank)
-            .collect(toCollection(() -> new TreeSet<>(String.CASE_INSENSITIVE_ORDER)));
+    private Set<String> classesIn(Artifact artifact) {
+        URL url = toURL(artifact);
+        Set<String> classes = Collections.emptySet();
 
-        pattern.splitAsStream(excludeScope)
-            .filter(StringUtils::isNotBlank)
-            .forEach(t -> scope.remove(t));
+        try {
+            JarFile jar = ((JarURLConnection) url.openConnection()).getJarFile();
 
-        if (scope.isEmpty()) {
-            log.warn("Specified scope is empty");
-            log.debug("    includeScope = {}", includeScope);
-            log.debug("    excludeScope = {}", excludeScope);
+            classes =
+                jar.stream()
+                .map(JarEntry::getName)
+                .map(PATTERN::matcher)
+                .filter(t -> t.matches())
+                .map(t -> t.group("path"))
+                .map(t -> t.replaceAll("/", "."))
+                .collect(toSet());
+        } catch (ZipException exception) {
+        } catch (IOException exception) {
+            log.debug("{}: {}", artifact, exception.getMessage(), exception);
         }
 
-        return scope;
+        return classes;
+    }
+
+    private URL toURL(Artifact artifact) {
+        URL url = null;
+
+        try {
+            url = new URI("jar", artifact.getFile().toURI().toASCIIString() + "!/", null).toURL();
+        } catch(URISyntaxException | MalformedURLException exception) {
+            log.debug("{}: {}", artifact, exception.getMessage(), exception);
+            throw new IllegalStateException(exception);
+        }
+
+        return url;
     }
 }
